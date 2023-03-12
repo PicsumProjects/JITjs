447fe5fa260b28dc0c85b42044f39026314da553
Codezigineer <53050996+Codezigineer@users.noreply.github.com>
2023-03-12 16:47:08 -0700

    Create defs&utils.ts

---
+++ src/defs&utils.ts
@@ -0,0 +1,11 @@
+export const LASTINSTR = 142;
+export type Instr = {
+  type: number = -1;
+  args: number[] = [];
+};
+function checkInstr(instr: Instr): void
+{
+  for(const arg of instr.args)
+    if((((arg >>> 0) > 64) || ((arg >>> 0) !== arg)) && (!((instr.type > 75) && (instr.type < 84)))) throw new Error('Invalid instruction');
+  if((instr.type >>> 0) > LASTINSTR) throw new Error('Invalid instruction');
+};      
